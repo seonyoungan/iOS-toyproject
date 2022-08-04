33600c9111c0a3bbd0039d203e1a47620a575554
seonyoungan <asy093@naver.com>
2022-08-05 02:39:42 +0900

    study screen transition basic

---
+++ screen-transition-example/screen-transition-example/SuguePushViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SuguePushViewController.swift
+//  screen-transition-example
+//
+//  Created by 안선영 on 2022/08/05.
+//
+
+import UIKit
+
+class SuguePushViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ screen-transition-example/screen-transition-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  screen-transition-example
+//
+//  Created by 안선영 on 2022/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
