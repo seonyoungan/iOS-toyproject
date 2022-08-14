efc236416faddd3bf16f8bbf1a7d0c79b481b0aa
seonyoungan <asy093@naver.com>
2022-08-14 18:40:08 +0900

    add: calc UI design

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by 안선영 on 2022/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
