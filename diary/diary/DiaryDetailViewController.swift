a5459c35af6e9e335acd8dc6dbafde325ce3e535
seonyoungan <asy093@naver.com>
2022-08-21 17:59:57 +0900

    add: detail diary UI Design

---
+++ diary/diary/DiaryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryDetailViewController.swift
+//  diary
+//
+//  Created by 안선영 on 2022/08/21.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
